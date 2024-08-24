ce4268f19fd97e20507e6e63671b51edf7028808
Bayram Yelec <bayrammyelec@gmail.com>
2024-08-24 13:24:52 +0300

    Initial Commit

---
+++ DailyTaskApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyTaskApp
+//
+//  Created by Bayram Yeleç on 24.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyTaskApp/DailyTaskAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTaskAppApp.swift
+//  DailyTaskApp
+//
+//  Created by Bayram Yeleç on 24.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DailyTaskAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
